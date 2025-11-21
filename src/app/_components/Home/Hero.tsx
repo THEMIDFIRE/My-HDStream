ac451b163af8d563ce6f51c2ebe10491a1c2f693
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2025-11-21 03:30:46 +0200

    added overlay

--- src/app/_components/Home/Hero.tsx
+++ src/app/_components/Home/Hero.tsx
@@ -5,7 +5,8 @@ export default function Hero() {
     <header className="min-h-screen">
       <div className="container bg-center relative">
         <div className="h-screen bg-[url('/hero-bg.png')] bg-no-repeat bg-cover bg-center"></div>
-        <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+        <div className="overlay absolute top-0 left-0 w-full h-2/3 bg-linear-to-b from-black/90 to-transparent"></div>
+        <div className="overlay absolute bottom-0 left-0 w-full h-2/3 bg-linear-to-t from-black/90 to-transparent"></div>
         <div className="logo-overlay bg-[url('/hero-logo.png')] bg-no-repeat inset-0 absolute bg-center bg-size-[90%] lg:bg-size-[25%]"></div>
       </div>
     </header>
