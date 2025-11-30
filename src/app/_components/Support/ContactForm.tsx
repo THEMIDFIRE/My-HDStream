195d1d11733f9508d58533702323eeb4cfac8d78
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2025-11-30 04:41:29 +0200

    +Contact Form - Update Spacing

---
+++ src/app/_components/Support/ContactForm.tsx
@@ -0,0 +1,8 @@
+
+export default function ContactForm() {
+    return (
+        <section className="mb-20 md:mb-28 2xl:mb-32">
+            <div className="container max-w-11/12 md:max-w-4/5 mx-auto"></div>
+        </section>
+    )
+}
